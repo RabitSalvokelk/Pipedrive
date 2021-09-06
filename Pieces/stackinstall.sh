01bc7e2dc32147e458293e63ef2424b5833fd0f3
RabitSalvokelk <Salongrobert@gmail.com>
2021-09-06 18:54:06 +0300

    updates in stackinstall.sh and README.md

--- Pieces/stackinstall.sh
+++ Pieces/stackinstall.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+#define variables for use
+clustername="elk-cluster"
+nodename="elkmstr"
+networkhost="172.19.175.130"
+networkport="9200"
+elasticsearchhosts=$(echo "http://" $networkhost ":" $networkport | tr -d ' ')
+
 #install java
 sudo apt-get install default-jre -y
 #wget elastic
@@ -12,6 +19,20 @@ sudo apt-get install elasticsearch -y
 sudo apt-get install kibana -y
 #install logstash
 sudo apt-get install logstash -y
+
+#elasticsearch configuration
+echo cluster.name: $clustername 								>> /etc/elasticsearch/elasticsearch.yml
+echo node.name: $nodename									>> /etc/elasticsearch/elasticsearch.yml
+echo node.data: true										>> /etc/elasticsearch/elasticsearch.yml
+echo network.host: $networkhost									>> /etc/elasticsearch/elasticsearch.yml
+echo http.port: $networkport									>> /etc/elasticsearch/elasticsearch.yml
+echo discovery.zen.ping.unicast.hosts: ['"192.168.9.2"','"192.168.9.3"','"192.168.9.4"']	>> /etc/elasticsearch/elasticsearch.yml
+echo discovery.zen.minimum_master_nodes: 2							>> /etc/elasticsearch/elasticsearch.yml
+#kibana configuration
+echo server.host: "0.0.0.0" 									>> /etc/kibana/kibana.yml
+echo elasticsearch.hosts: $elasticsearchhosts							>> /etc/kibana/kibana.yml
+#logstash configuration
+
 #restart all services and enable on boot
 sudo systemctl restart logstash
 sudo systemctl enable logstash
